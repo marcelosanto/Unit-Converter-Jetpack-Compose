d2422fa67a2fe02236e26984635abd8ac1a09dc7
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-07 22:15:46 -0300

    ConversionMenu component

--- app/src/main/java/xyz/marcelo/androidapptemplate/ConversionMenu.kt
+++ app/src/main/java/xyz/marcelo/androidapptemplate/ConversionMenu.kt
@@ -1,7 +1,69 @@
 package xyz.marcelo.androidapptemplate
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+import xyz.marcelo.androidapptemplate.model.Conversion
 
 @Composable
-fun ConversionMenu() {
+fun ConversionMenu(list: List<Conversion>, modifier: Modifier = Modifier) {
+    val displayingText = remember { mutableStateOf("Selecione o tipo de conversão") }
+    val textFieldSize = remember { mutableStateOf(Size.Zero) }
+    val expanded = remember { mutableStateOf(false) }
+    val icon = if (expanded.value) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown
+
+    Column {
+        OutlinedTextField(
+            value = displayingText.value,
+            onValueChange = { displayingText.value = it },
+            textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+            modifier = modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coord ->
+                    textFieldSize.value = coord.size.toSize()
+                },
+            label = { Text(text = "Tipos de Conversões") },
+            trailingIcon = {
+                Icon(imageVector = icon, contentDescription = "icon",
+                    modifier.clickable { expanded.value = !expanded.value })
+            },
+            readOnly = true
+        )
+
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false },
+            modifier = modifier.width(with(LocalDensity.current) { textFieldSize.value.width.toDp() })
+        ) {
+            list.forEach { conversion ->
+                DropdownMenuItem(onClick = {
+                    displayingText.value = conversion.description
+                    expanded.value = false
+                }) {
+                    Text(
+                        text = conversion.description,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/xyz/marcelo/androidapptemplate/view/TopScreen.kt
+++ app/src/main/java/xyz/marcelo/androidapptemplate/view/TopScreen.kt
@@ -1,8 +1,10 @@
 package xyz.marcelo.androidapptemplate.view
 
 import androidx.compose.runtime.Composable
+import xyz.marcelo.androidapptemplate.ConversionMenu
 import xyz.marcelo.androidapptemplate.model.Conversion
 
 @Composable
 fun TopScreen(list: List<Conversion>) {
+    ConversionMenu(list = list)
 }

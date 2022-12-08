6b5ee499ba5fbce7107e5a7bbd91542ef9c1c9a5
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-08 09:07:42 -0300

    ResultBlock show result from conversions

--- app/src/main/java/xyz/marcelo/androidapptemplate/view/TopScreen.kt
+++ app/src/main/java/xyz/marcelo/androidapptemplate/view/TopScreen.kt
@@ -1,13 +1,15 @@
 package xyz.marcelo.androidapptemplate.view
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import xyz.marcelo.androidapptemplate.ConversionMenu
 import xyz.marcelo.androidapptemplate.InputBlock
+import xyz.marcelo.androidapptemplate.ResultBlock
 import xyz.marcelo.androidapptemplate.model.Conversion
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 @Composable
 fun TopScreen(list: List<Conversion>) {
@@ -19,16 +21,32 @@ fun TopScreen(list: List<Conversion>) {
         mutableStateOf("")
     }
 
+    val typedValue = remember {
+        mutableStateOf("0.0")
+    }
+
     ConversionMenu(list = list) {
         selectedConversion.value = it
     }
 
     selectedConversion.value?.let {
         InputBlock(conversion = it, inputText = inputText) { input ->
-            Log.i(
-                "TAG",
-                "TopScreen: $input - ${it.multiplyBy} = ${input.toDouble() * it.multiplyBy}"
-            )
+            typedValue.value = input
         }
+    }
+
+    if (typedValue.value != "0.0") {
+        val input = typedValue.value.toDouble()
+        val multiply = selectedConversion.value!!.multiplyBy
+        val result = input * multiply
+
+        //arrendondar resultado
+        val df = DecimalFormat("#.####")
+        df.roundingMode = RoundingMode.DOWN
+        val roundedResult = df.format(result)
+
+        val message1 = "${typedValue.value} ${selectedConversion.value!!.convertFrom} é igual a"
+        val message2 = "$roundedResult ${selectedConversion.value!!.convertTo}"
+        ResultBlock(message1 = message1, message2 = message2)
     }
 }
